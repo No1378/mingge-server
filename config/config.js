65d717aa3ba8ef682655e0276c3dab32d2db80a3
fuming <fmuncle@163.com>
2020-08-19 10:53:50 +0800

    '修改了配置默认值'

--- config/config.js
+++ config/config.js
@@ -5,5 +5,5 @@ module.exports = {
     "root": argv.root || process.cwd(),  // 网站根目录 没有指定使用命令所在的目录
     "port": argv.port || 8000, // 端口
     "cache-control": "max-age=0",  // 缓存控制
-    "auto-open": false  // 是否自动打开浏览器
+    "auto-open": true  // 是否自动打开浏览器
 }
